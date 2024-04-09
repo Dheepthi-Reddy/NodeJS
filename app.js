e656b7a52c580d407b9590689db940f6ede99acb
Dheepthi-Reddy <dheepthireddyv@gmail.com>
2024-04-09 10:41:29 -0500

    Automatic reloading, default logging, error handling

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 // app.use((req, res, next) => {
 //     res.status(200).json({
 //         message: "It Works!!!"
 //     });
 // } );
+
+app.use(morgan('dev'));
 
 const productRoutes = require('./api/routes/products');
 
@@ -16,5 +19,20 @@ app.use('/products', productRoutes); // productRoutes is a handler
 const orderRoutes = require('./api/routes/orders');
 
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
